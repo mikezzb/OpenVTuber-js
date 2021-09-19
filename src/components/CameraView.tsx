d1f7b9e061e78ff8fb8bea89a06f820e836c6bd3
mikezzb <mikezhoudev@gmail.com>
2021-09-19 15:44:43 +0800

    Embed webcam in corner

---
+++ src/components/CameraView.tsx
@@ -0,0 +1,25 @@
+import { FC, RefObject } from 'react';
+import '@tensorflow/tfjs-backend-webgl';
+import Webcam from 'react-webcam';
+import './CameraView.scss';
+
+type Props = {
+  webcamRef: RefObject<Webcam>;
+  meshCanvasRef: RefObject<HTMLCanvasElement>;
+};
+
+const CameraView: FC<Props> = ({ webcamRef, meshCanvasRef }) => {
+  return (
+    <>
+      <Webcam className="webcam-video" ref={webcamRef} />
+      <canvas
+        width={320}
+        height={240}
+        className="facemesh-canvas"
+        ref={meshCanvasRef}
+      />
+    </>
+  );
+};
+
+export default CameraView;
